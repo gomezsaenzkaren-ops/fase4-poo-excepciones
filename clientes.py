11e0924d52bf6017ceff0f42e9a1216298456f89
Grupo_213023_KarenGómez <gomezsaenzkaren@gmail.com>
2026-05-04 17:37:37 -0500

    Update clientes.py

--- clientes.py
+++ clientes.py
@@ -1,1 +1,26 @@
+from excepciones import ClienteError
 
+class Cliente:
+    def __init__(self, nombre, documento, correo):
+        try:
+            if not nombre.replace(" ", "").isalpha():
+                raise ClienteError("El nombre solo debe contener letras")
+
+            if len(documento) < 5:
+                raise ClienteError("El documento debe tener al menos 5 caracteres")
+
+            if "@" not in correo:
+                raise ClienteError("El correo no es válido")
+
+            self.__nombre = nombre
+            self.__documento = documento
+            self.__correo = correo
+
+        except Exception as e:
+            raise ClienteError("Error al crear el cliente") from e
+
+    def get_nombre(self):
+        return self.__nombre
+
+    def get_documento(self):
+        return self.__documento
