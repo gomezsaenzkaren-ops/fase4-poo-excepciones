90240dcfa6a5adce6c7061774b1a6edf9610552d
Grupo_213023_KarenGómez <gomezsaenzkaren@gmail.com>
2026-05-04 17:39:25 -0500

    Update gestor.py

--- gestor.py
+++ gestor.py
@@ -1,1 +1,18 @@
+from reserva import Reserva
 
+class Gestor:
+    def __init__(self):
+        self.clientes = []
+        self.servicios = []
+        self.reservas = []
+
+    def agregar_cliente(self, cliente):
+        self.clientes.append(cliente)
+
+    def agregar_servicio(self, servicio):
+        self.servicios.append(servicio)
+
+    def crear_reserva(self, cliente, servicio, horas):
+        reserva = Reserva(cliente, servicio, horas)
+        self.reservas.append(reserva)
+        return reserva
