e9d723ab12a347cd5e08cf0f0fa385d410c2034c
Grupo_213023_KarenGómez <gomezsaenzkaren@gmail.com>
2026-05-04 17:38:59 -0500

    Update reserva.py

--- reserva.py
+++ reserva.py
@@ -1,1 +1,26 @@
+from excepciones import ReservaError
 
+class Reserva:
+    def __init__(self, cliente, servicio, horas):
+        self.cliente = cliente
+        self.servicio = servicio
+        self.horas = horas
+        self.estado = "Pendiente"
+
+    def confirmar(self):
+        self.estado = "Confirmada"
+
+    def cancelar(self):
+        self.estado = "Cancelada"
+
+    def procesar(self):
+        try:
+            costo = self.servicio.calcular_costo(self.horas)
+        except Exception as e:
+            self.estado = "Error"
+            raise ReservaError("Error al procesar la reserva") from e
+        else:
+            self.confirmar()
+            return costo
+        finally:
+            print("Proceso de reserva finalizado")
