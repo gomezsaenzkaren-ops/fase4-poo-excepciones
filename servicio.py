c7e1df809ad08a4964a4effb83f4e2b64e971b4a
Grupo_213023_KarenGómez <gomezsaenzkaren@gmail.com>
2026-05-04 17:38:00 -0500

    Update servicio.py

--- servicio.py
+++ servicio.py
@@ -1,1 +1,31 @@
+from abc import ABC, abstractmethod
+from excepciones import ServicioError
 
+class Servicio(ABC):
+    def __init__(self, nombre, costo_base):
+        self.nombre = nombre
+        self.costo_base = costo_base
+
+    @abstractmethod
+    def calcular_costo(self, horas, impuesto=0, descuento=0):
+        pass
+
+
+class ReservaSala(Servicio):
+    def calcular_costo(self, horas, impuesto=0, descuento=0):
+        costo = self.costo_base * horas
+        return costo + (costo * impuesto) - descuento
+
+
+class AlquilerEquipo(Servicio):
+    def calcular_costo(self, horas, impuesto=0, descuento=0):
+        costo = (self.costo_base * horas) + 20
+        return costo + (costo * impuesto) - descuento
+
+
+class Asesoria(Servicio):
+    def calcular_costo(self, horas, impuesto=0, descuento=0):
+        if horas < 1:
+            raise ServicioError("Las horas deben ser mayores a 0")
+        costo = (self.costo_base * horas) * 1.2
+        return costo + (costo * impuesto) - descuento
